113f5e7c2b463a13de2a79ce469b1cf085330b82
gamaleldeeb31121970-ctrl <gamaleldeeb31121970@gmail.com>
2026-01-30 03:05:23 +0200

    Remove Firebase configuration script

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,1 @@
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-database-compat.js"></script>
 
-<script>
-  const firebaseConfig = {
-    apiKey: "AIzaSy...خاصتك...",
-    authDomain: "your-project.firebaseapp.com",
-    databaseURL: "https://your-project-default-rtdb.firebaseio.com",
-    projectId: "your-project",
-    storageBucket: "your-project.appspot.com",
-    messagingSenderId: "1234567890",
-    appId: "1:1234567890:web:abcdefg"
-  };
-
-  firebase.initializeApp(firebaseConfig);
-  const database = firebase.database();
-</script>
