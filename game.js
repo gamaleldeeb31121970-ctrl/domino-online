07fd87a499731a907d78bbecb447537ca968e40e
gamaleldeeb31121970-ctrl <gamaleldeeb31121970@gmail.com>
2026-01-30 08:47:36 +0200

    Add turn locking mechanism for player actions

--- game.js
+++ game.js
@@ -2,13 +2,14 @@ let players = [];
 let board = [];
 let turn = 0;
 let playerCount = 2;
+let locked = false;
 
 function generateDomino() {
   let d = [];
-  for (let i=0;i<=6;i++)
-    for (let j=i;j<=6;j++)
-      d.push({l:i,r:j});
-  return d.sort(()=>Math.random()-0.5);
+  for (let i = 0; i <= 6; i++)
+    for (let j = i; j <= 6; j++)
+      d.push({ l: i, r: j });
+  return d.sort(() => Math.random() - 0.5);
 }
 
 function startGame(count) {
@@ -19,9 +20,9 @@ function startGame(count) {
   const domino = generateDomino();
   players = [];
 
-  for (let i=0;i<count;i++) {
+  for (let i = 0; i < count; i++) {
     players.push({
-      hand: domino.splice(0,7),
+      hand: domino.splice(0, 7),
       ai: i !== 0
     });
   }
@@ -37,7 +38,8 @@ function render() {
 
   const boardDiv = document.getElementById("board");
   boardDiv.innerHTML = "";
-  board.forEach(p=>{
+
+  board.forEach(p => {
     let img = document.createElement("img");
     img.src = `assets/tiles/tile_${p.l}_${p.r}.png`;
     img.className = "piece";
@@ -47,22 +49,24 @@ function render() {
   const handDiv = document.getElementById("hand");
   handDiv.innerHTML = "";
 
-  players[0].hand.forEach((p,i)=>{
+  players[0].hand.forEach((p, i) => {
     let img = document.createElement("img");
     img.src = `assets/tiles/tile_${p.l}_${p.r}.png`;
     img.className = "piece";
-    img.onclick = ()=>play(i);
+    img.onclick = () => play(i);
     handDiv.appendChild(img);
   });
 
   if (players[turn].ai) {
-    setTimeout(aiPlay, 1000);
+    setTimeout(aiPlay, 800);
   }
 }
 
 function play(i) {
-  if (turn !== 0) return;
-  board.push(players[0].hand.splice(i,1)[0]);
+  if (turn !== 0 || locked) return;
+  locked = true;
+
+  board.push(players[0].hand.splice(i, 1)[0]);
   nextTurn();
 }
 
@@ -76,6 +80,6 @@ function aiPlay() {
 
 function nextTurn() {
   turn = (turn + 1) % playerCount;
+  locked = false;
   render();
 }
-
