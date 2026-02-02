453b5e84e2dd121e2cbee25e801fa115f5b4baa9
gamaleldeeb31121970-ctrl <gamaleldeeb31121970@gmail.com>
2026-02-03 00:26:47 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,8 +1,1 @@
-games/domino/
-├── index.html
-├── js/
-│   ├── firebase.js      <-- اتركه كما هو
-│   └── main.js          <-- النسخة الجديدة جاهزة 3D
-├── css/
-│   └── style.css        <-- اتركها كما هي
-├── leaderboard.html     <-- اتركها كما هي
+
